261f51dca74961a8d3b799a66c8fce5a53222e41
YousufRaza1 <yousuf.ice17@gmail.com>
2023-12-18 22:56:31 +0600

    Project added

---
+++ Advanced-Note-Taking1/Advanced_Note_Taking1App.swift
@@ -0,0 +1,17 @@
+//
+//  Advanced_Note_Taking1App.swift
+//  Advanced-Note-Taking1
+//
+//  Created by Yousuf on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Advanced_Note_Taking1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
